8c7bbaeb0022cfae408f613cef6e7ac882bf6bca
Rika4698 <rikasharmin181@gmail.com>
2023-10-21 22:41:05 +0600

    add someting

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i5g3jew.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://rikasharmin181:bw5OjaY4O9hHDB5C@cluster0.i5g3jew.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,7 +27,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const productCollection = client.db('productDB').collection("product");
     
     const sliderCollection = client.db('sliderDB').collection("slider.json");
@@ -127,7 +127,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
