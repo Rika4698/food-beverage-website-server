6282c9216a197785dac22bd0d0738e8ff06c2170
Rika4698 <rikasharmin181@gmail.com>
2023-10-31 22:06:57 +0600

    somthing add

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://rikasharmin181:bw5OjaY4O9hHDB5C@cluster0.i5g3jew.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i5g3jew.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
